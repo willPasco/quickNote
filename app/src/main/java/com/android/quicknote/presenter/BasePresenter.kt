3e7ab476cbf5141538e08507355ade2aad7cc1c8
Azevedo,William <will.pasco.az@gmail.com>
2019-10-12 20:15:41 -0300

    Getting the dao in presenter.

--- app/src/main/java/com/android/quicknote/presenter/BasePresenter.kt
+++ app/src/main/java/com/android/quicknote/presenter/BasePresenter.kt
@@ -1,5 +1,8 @@
 package com.android.quicknote.presenter
 
+import android.content.Context
+import com.android.quicknote.dao.QuickNoteDao
+import com.android.quicknote.db.QuickNoteDatabase
 import com.android.quicknote.repository.MainRepository
 import com.android.quicknote.view.BaseView
 
@@ -7,8 +10,11 @@ open class BasePresenter {
 
     private val repository = MainRepository()
     private lateinit var view: BaseView
+    private var dao: QuickNoteDao? = null
 
     fun attachView(view: BaseView){
         this.view = view
+        val context = view as Context
+        QuickNoteDatabase.getDatabase(context)
     }
 }
