a49444a1131ace226c111fa16792b36da54ddb62
Azevedo,William <will.pasco.az@gmail.com>
2019-10-13 17:11:11 -0300

    Overriding a toString method in dataWrapper.

--- app/src/main/java/com/android/quicknote/data/util/DataWrapper.kt
+++ app/src/main/java/com/android/quicknote/data/util/DataWrapper.kt
@@ -5,4 +5,9 @@ class DataWrapper<T> {
     var data: T? = null
     var message: String? = null
     var code: Int? = null
+
+
+    override fun toString(): String {
+        return "DataWrapper(data=$data, message=$message, code=$code)"
+    }
 }
