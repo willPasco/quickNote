b841893067db33b0d7e538165b7529a2d516765f
Azevedo,William <will.pasco.az@gmail.com>
2019-10-13 16:44:51 -0300

    Creating the insert method to add new tasks in database.

--- app/src/main/java/com/android/quicknote/data/dao/QuickNoteDao.kt
+++ app/src/main/java/com/android/quicknote/data/dao/QuickNoteDao.kt
@@ -1,8 +1,17 @@
 package com.android.quicknote.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import com.android.quicknote.data.entity.TaskEntity
 
 @Dao
 interface QuickNoteDao {
 
+    companion object{
+        const val SUCCESS_CODE = 500
+        const val ERROR_CODE = 400
+    }
+
+    @Insert
+    fun insertTask(taskEntity: TaskEntity)
 }
