e9e6dd0123c9e758f9a4f1a3c95d81c60f31a053
Azevedo,William <will.pasco.az@gmail.com>
2019-10-13 17:26:42 -0300

    Changing the table of task entity.

--- app/src/main/java/com/android/quicknote/data/entity/TaskEntity.kt
+++ app/src/main/java/com/android/quicknote/data/entity/TaskEntity.kt
@@ -4,5 +4,5 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "TaskEntity")
+@Entity(tableName = "task_table")
  class TaskEntity(@PrimaryKey var id: Long, @ColumnInfo(name = "title") var title:String)
