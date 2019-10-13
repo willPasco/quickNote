012fef9fcf5a6377a35acb75e44def79a8cd2b8f
Azevedo,William <will.pasco.az@gmail.com>
2019-10-13 16:44:29 -0300

    Renaming the main repository to task repository.

--- app/src/main/java/com/android/quicknote/presenter/MainPresenter.kt
+++ app/src/main/java/com/android/quicknote/presenter/MainPresenter.kt
@@ -2,15 +2,15 @@ package com.android.quicknote.presenter
 
 import android.content.Context
 import com.android.quicknote.base.BasePresenter
-import com.android.quicknote.data.repository.MainRepository
+import com.android.quicknote.data.repository.TaskRepository
 import com.android.quicknote.view.MainView
 
 class MainPresenter : BasePresenter<MainView>() {
 
-    var repository: MainRepository? = null
+    var repository: TaskRepository? = null
 
     override fun attachView(view: MainView) {
         super.attachView(view)
-        repository = MainRepository(view as Context)
+        repository = TaskRepository(view as Context)
     }
 }
